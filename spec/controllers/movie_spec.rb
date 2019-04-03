bcf32becc9f46b3b6353fe9a0b391e352b5601b2
andbri <andbri@gmail.com>
2019-04-03 07:08:40 -0300

    testes nos controllers controller all_movies_spec teste (actions: #index,#show) controller movie_spec teste (CRUD)

---
+++ spec/controllers/movie_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+RSpec.describe MoviesController, type: :controller do
+  describe 'User with account' do
+    let(:user) { User.create!(email: 'a@gmail.com', password:'123123') }
+    let(:movie) { Movie.create!(user: user,description:'des') }
+    before(:each) do
+      sign_in(user)
+
+      @request.env['devise.mapping'] = Devise.mappings[:user]
+    end
+
+    describe 'GET #index' do
+      it 'returns http success' do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe 'GET #create' do
+      it 'returns http success' do
+        post :create, params: {
+          movie: {description: 'filme 1'}
+        }
+        expect(response).to have_http_status(302)
+        expect(Movie.last.description).to eq('filme 1')
+      end
+    end
+
+    describe 'GET #update' do
+      it 'returns http success' do
+    movie.id
+        put :update, params: { id: movie.id,
+          movie: {id: movie.id,description: 'des2'}
+        }
+        expect(response).to have_http_status(302)
+        expect(movie.reload.description).to eq('des2')
+      end
+    end
+
+    describe '#destroy' do
+      it 'redirect' do
+          delete :destroy, params: { id: movie.id }
+          expect(response).to have_http_status(302)
+      end
+    end
+  end
+end
